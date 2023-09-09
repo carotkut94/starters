13fa560d6bc7d35cc8664eb912583f9057d31b1b
carotkut94 <carotkut12@gmail.com>
2023-09-09 11:31:06 +0530

    Initial Commit

---
+++ FoodBazaar/FoodBazaarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodBazaarApp.swift
+//  FoodBazaar
+//
+//  Created by Deathcode on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodBazaarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
