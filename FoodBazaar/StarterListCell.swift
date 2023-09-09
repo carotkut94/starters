f105224a7ae4d61ec8451af33c7216a0032d0958
carotkut94 <carotkut12@gmail.com>
2023-09-09 23:26:34 +0530

    feat: uses async image instead of custom AppImageView

--- FoodBazaar/StarterListCell.swift
+++ FoodBazaar/StarterListCell.swift
@@ -13,14 +13,28 @@ struct StarterListCell: View {
     
     var body: some View {
         HStack {
-            AppImageView(urlString:starter.imageURL)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 120, height: 90)
-                .cornerRadius(10)
+//            AppImageView(urlString:starter.imageURL)
+//                .aspectRatio(contentMode: .fit)
+//                .frame(width: 120, height: 90)
+//                .cornerRadius(10)
+            
+            AsyncImage(url: URL(string: starter.imageURL)){ image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120, height: 90)
+                    .cornerRadius(8)
+            } placeholder: {
+                 Image("food-placeholder")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 120, height: 90)
+                    .cornerRadius(8)
+            }
             
             VStack(alignment: .leading, spacing: 5) {
                 Text(starter.name)
-                    .font(.title)
+                    .font(.title2)
                     .fontWeight(.medium)
                 Text("$\(starter.price, specifier: "%.2f")")
                     .foregroundColor(.secondary)
